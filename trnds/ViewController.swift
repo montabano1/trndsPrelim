95596943a175d3e86b342ccc6b04ed38f0d35c0b
Michael Montalbano <montabano1@gmail.com>
2020-06-05 20:47:53 -0400

    Initial Commit

---
+++ trnds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trnds
+//
+//  Created by michael montalbano on 6/5/20.
+//  Copyright © 2020 michael montalbano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
